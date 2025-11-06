84bd6b75a320e4c49222b29d7b4f84fcc4ea5583
Niraja85 <tripathi.niraja@gmail.com>
2025-11-06 23:29:14 +0100

    Added newsletter app, model, views and url

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, reverse
+from django.contrib import messages
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from .models import Newsletter
 
 
@@ -6,6 +9,25 @@ from .models import Newsletter
 def subscribe(request):
     if request.method == 'POST':
         email = request.POST.get('email', None)
+
+        if not email:
+            messages.error(request, "You must type a valid email address")
+            return redirect(reverse, ('home'))
+        try:
+            validate_email(email)
+        except ValidationError as e:
+            messages.error(request, e.messages[0])
+            return redirect("/")
+
+        subscribe_model_instance = Newsletter()
+        subscribe_model_instance.email = email
+        subscribe_model_instance.save()
+
+        messages.success(request, "Thank you for subscribing!")
+        return redirect(request.META.get("HTTP_REFERER", "/"))
+
+
+
 
 
 
