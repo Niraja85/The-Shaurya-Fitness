6c47f44aae0f88259a7b3f058410cb679d23016c
Niraja85 <tripathi.niraja@gmail.com>
2025-11-14 11:44:40 +0100

    Added allauth middleware

--- main/settings.py
+++ main/settings.py
@@ -74,8 +74,10 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 
 ]
 
