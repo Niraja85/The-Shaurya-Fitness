789bf2140fc5fb28fe426c9a684f7d8adf01b194
Niraja85 <tripathi.niraja@gmail.com>
2025-11-17 18:34:13 +0100

    fix -added  https support for checkout in heroku

--- main/settings.py
+++ main/settings.py
@@ -236,3 +236,9 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+if 'DYNO' in os.environ:  # testing if checkout works on Heroku with https
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
